7c7e5f83ef43e14e4124c9c6d343f514ed4c568c
K4UNG <kgzinhein.my@gmail.com>
2022-08-17 14:01:24 +0630

    tablet hero section

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -1,19 +1,38 @@
-import styles from './Featured.module.css'
-import FeaturedItem from './FeaturedItem'
+import styles from "./Featured.module.css";
+import FeaturedItem from "./FeaturedItem";
+import { Link } from "react-router-dom";
 
 export default function Featured() {
-    return <div className={styles.featured}>
-        <h2 className={styles.title}>FEATURED PROJECTS</h2>
+  return (
+    <div className={styles.featured}>
+      <h2 className={styles.title}>FEATURED PROJECTS</h2>
 
-        <FeaturedItem 
-            number='01'
-            title="tip calculator"
-            src="./images/tip.png"
-            live="/"
-            repo="/"
-            id="someid"
-            difficulty="Intermediate"
-            tags={['React', 'CSS']}
-        />
+      <FeaturedItem
+        number="01"
+        title="tip calculator"
+        src="./images/tip.png"
+        live="/"
+        repo="/"
+        id="someid"
+        difficulty="Intermediate"
+        tags={["React", "CSS"]}
+      />
+      <FeaturedItem
+        number="02"
+        title="tip calculator"
+        src="./images/tip.png"
+        live="/"
+        repo="/"
+        id="someid"
+        difficulty="Intermediate"
+        tags={["React", "CSS"]}
+      />
+
+      <div className={styles.all__wrapper}>
+        <Link to="/projects" className={styles.all}>
+          see all projects
+        </Link>
+      </div>
     </div>
+  );
 }
